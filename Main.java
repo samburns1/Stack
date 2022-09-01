2464e5e37b9063e9ff7c7140bca6163ebcaca5a3
Sammy Burns <sburns2023@kehillahstudent.org>
2022-08-31 22:03:46 -0700

    first 2 tests, pop and push

--- Main.java
+++ Main.java
@@ -1,1 +1,35 @@
+import java.util.logging.Logger;
+import java.util.logging.Level;
+class Main{
+    private static Logger LOGGER = Logger.getLogger(Main.class.getName());
+    static int pass = 0;
+    static int fail = 0;
+    public static void main(String[]args){
+        System.out.println("hello world");
 
+        testConstructStack();
+        testPush();
+        System.out.println(" Passed: " + pass + "|| Failed: " + fail);
+    }
+
+
+    static void testConstructStack() {
+        Stack dishes = new Stack(5);
+        if(dishes.empty()) pass++;
+        else {
+            fail++;
+            LOGGER.log(Level.WARNING, "Failed stack constructor, expected empty got not empty");
+        }
+    }
+
+
+    static void testPush(){
+        Stack dishes = new Stack(5);
+        dishes.push(31);
+        if(!dishes.empty()) pass++;
+        else {
+            fail++;
+            LOGGER.log(Level.WARNING, "Failed stack push, expected not empty got empty");
+        }
+    }
+}
--- Stack.java
+++ Stack.java
@@ -1,1 +1,30 @@
+public class Stack {
+    private int top;
+    private int stackarr[];
 
+    public Stack(int maxsize){
+        stackarr = new int[maxsize];
+        top = 0;
+    }
+
+    public int getTop(){
+      return top;
+    }
+
+    public boolean empty(){
+        return top == 0;
+    }
+
+    public void push(int onstack){
+        stackarr[top++] = onstack;
+    }
+
+    public int pop(){
+        int tmp = stackarr[top];
+
+
+        return tmp;
+    }
+
+
+}
